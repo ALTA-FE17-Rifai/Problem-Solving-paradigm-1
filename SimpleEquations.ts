510e6a979921473608e81fa994c8ccbf03c7c36e
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-24 18:55:41 +0700

    Problem 1 - Simple Equations

---
+++ SimpleEquations.ts
@@ -0,0 +1,11 @@
+function simpleEquations(a, b, c) {
+    
+    let (x + y + z = A)
+    let (xyz = B)
+    let (x^2 + y^2 + z^2 = c)
+
+    simpleEquations(1, 2, 3)
+    simpleEquations(6, 6, 14)
+
+    console.log(123)
+    console.log(123)
