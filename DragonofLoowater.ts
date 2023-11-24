22817e9eea3a5c3c93a671154ba40a54aa5e0c3b
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-24 18:56:38 +0700

    Problem 2 - Dragon of Loowater

---
+++ DragonofLoowater.ts
@@ -0,0 +1,18 @@
+function dragonOfLoowater(dragonHead, knightHeight) {
+
+    let [5, 4], [7, 8, 4]
+    console.log(11)
+
+    let [5, 10], [5]
+    console.log("knight fall")
+
+    let [7, 2], [4, 3, 1, 2]
+    console.log("knight fall")
+
+    let [7, 2], [2, 1, 8, 5]
+    console.log(10)
+
+    dragonOfLoowater([5, 4], [7, 8, 4])
+    dragonOfLoowater([5, 10], [5])
+    dragonOfLoowater([7, 2], [4, 3, 1, 2])
+    dragonOfLoowater([7, 2], [2, 1, 8, 5])
