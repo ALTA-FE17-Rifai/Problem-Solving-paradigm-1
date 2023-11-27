fa3acb668c2ef8bfa452c18f153e0920199b7213
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-27 19:19:22 +0700

    Problem 4 - Roman Numerals

---
+++ RomanNumerals.ts
@@ -0,0 +1,18 @@
+function romanNumerals(value ) {
+
+    const input = 4
+    console.log ("IV");
+    const input = 9
+    console.log ("IX")
+    const input = 23
+    console.log ("XXIII")
+    const input = 2021
+    console.log ("MMXXXI")
+    const input = 1646
+    console.log ("MDCXLVI")
+
+    console.log(romanNumerals(6))
+    console.log(romanNumerals(9))
+    console.log(romanNumerals(23))
+    console.log(romanNumerals(2021))
+    console.log(romanNumerals(1646))
